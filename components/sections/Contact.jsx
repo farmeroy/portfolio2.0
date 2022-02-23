69b9240fd47af311534c3a19be42e1b6c08ffc8a
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-02-23 20:47:21 +0100

    created ContactForm

---
+++ components/sections/Contact.jsx
@@ -0,0 +1,64 @@
+import { Box, TextField } from '@mui/material';
+
+const ContactForm = () => {
+  return(
+    <Box
+      component="form"
+    >
+       <TextField
+          required
+          id="filled-required"
+          label="Required"
+          defaultValue="Hello World"
+          variant="filled"
+        />
+        <TextField
+          disabled
+          id="filled-disabled"
+          label="Disabled"
+          defaultValue="Hello World"
+          variant="filled"
+        />
+        <TextField
+          id="filled-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          variant="filled"
+        />
+        <TextField
+          id="filled-read-only-input"
+          label="Read Only"
+          defaultValue="Hello World"
+          InputProps={{
+            readOnly: true,
+          }}
+          variant="filled"
+        />
+        <TextField
+          id="filled-number"
+          label="Number"
+          type="number"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          variant="filled"
+        />
+        <TextField
+          id="filled-search"
+          label="Search field"
+          type="search"
+          variant="filled"
+        />
+        <TextField
+          id="filled-helperText"
+          label="Helper text"
+          defaultValue="Default Value"
+          helperText="Some important text"
+          variant="filled"
+        />
+      </Box>
+  )
+}
+
+export default ContactForm;
