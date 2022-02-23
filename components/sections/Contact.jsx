4a62887d4dadaecdb9d6ba61340085e3c1d3e863
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-02-23 21:16:12 +0100

    added paper for form background

--- components/sections/Contact.jsx
+++ components/sections/Contact.jsx
@@ -1,64 +1,30 @@
-import { Box, TextField } from '@mui/material';
+import { Paper, Box, TextField } from "@mui/material";
 
 const ContactForm = () => {
-  return(
-    <Box
-      component="form"
-    >
-       <TextField
-          required
-          id="filled-required"
-          label="Required"
-          defaultValue="Hello World"
-          variant="filled"
-        />
-        <TextField
-          disabled
-          id="filled-disabled"
-          label="Disabled"
-          defaultValue="Hello World"
-          variant="filled"
-        />
-        <TextField
-          id="filled-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          variant="filled"
-        />
-        <TextField
-          id="filled-read-only-input"
-          label="Read Only"
-          defaultValue="Hello World"
-          InputProps={{
-            readOnly: true,
-          }}
-          variant="filled"
-        />
-        <TextField
-          id="filled-number"
-          label="Number"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="filled"
-        />
-        <TextField
-          id="filled-search"
-          label="Search field"
-          type="search"
-          variant="filled"
-        />
-        <TextField
-          id="filled-helperText"
-          label="Helper text"
-          defaultValue="Default Value"
-          helperText="Some important text"
-          variant="filled"
-        />
-      </Box>
-  )
-}
+  return (
+    <Box component="form" sx={{display:'flex', flexDirection:'column', width:500}}>
+      <Paper>
+      <TextField
+        required
+        fullWidth
+        id="filled-required"
+        type="email"
+        label="email address"
+        defaultValue="email@email.com"
+        variant="filled"
+      />
+      <TextField
+        required
+        fullWidth
+        id="your-message-required"
+        label="your message"
+        type="text"
+        defaultValue="Send me a message..."
+        variant="filled"
+      />
+</Paper>
+    </Box>
+  );
+};
 
 export default ContactForm;
