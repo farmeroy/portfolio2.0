8b9fa64bc1a3f231a5b91ed63607e216b156550e
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-02-23 12:00:07 +0100

    created ProjectCard

---
+++ components/UI/ProjectCards.jsx
@@ -0,0 +1,9 @@
+import { Card, CardContent, CardMedia, CardActionArea, CardActions, Button, Typography } from '@mui/material'
+import Image from 'next/image'
+
+
+const ProjectCard = (props) => {
+  <Card>
+
+  </Card>
+}
