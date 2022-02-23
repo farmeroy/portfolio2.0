7570852b6effe47553bf63e3fea9697a5f548207
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-02-23 20:47:11 +0100

    imported ContactForm

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import profile from "../public/raffe_profile.jpg";
 import Hero from '../components/sections/Hero'
 import About from '../components/sections/About'
 import Projects from '../components/sections/Projects'
+import ContactForm from '../components/sections/Contact'
 import {  Container } from '@mui/material'
 
 export default function Home() {
@@ -24,6 +25,7 @@ export default function Home() {
         </div>
         <About />
         <Projects />
+        <ContactForm />
                 <Container>
         <Image alt="a portrait of me" src={profile} />
         <Image alt="a portrait of me" src={profile} />
