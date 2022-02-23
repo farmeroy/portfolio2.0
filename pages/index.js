f9a29215fa3d60abfe2bb7d612effced702d1524
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-02-23 01:07:59 +0100

    import Typo... Button and began layout

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import HideNav from '../components/HideNav.jsx'
-import Navbar from '../components/Navbar'
-import profile from '../public/raffe_profile.jpg'
-
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Navbar from "../components/Navbar";
+import profile from "../public/raffe_profile.jpg";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper"
 
 export default function Home() {
   return (
@@ -12,7 +13,7 @@ export default function Home() {
       <Head>
         <title>Raffaele Cataldo - Developer</title>
         <meta name="description" content="built by Raffaele Cataldo" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/R.svg" />
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
 
@@ -20,19 +21,41 @@ export default function Home() {
       {/* <HideNav /> */}
 
       <main className={styles.main}>
-        <h1>Raffaele Cataldo</h1>
-        <h2>Web Developer</h2>
-        <Image alt="a portrait of me" src={profile}/>
-        <Image alt="a portrait of me" src={profile}/>
-        <Image alt="a portrait of me" src={profile}/>
-        <Image alt="a portrait of me" src={profile}/>
+        <div className={styles.hero}>
+ <Paper elevation={0} sx={{padding:20}}>
+ <Typography variant="h2" component="div" gutterBottom sx={{fontFamily: 'Noto Sans Mono'}}>
+          Raffaele Cataldo
+        </Typography>
+        <Typography variant="h3" component="div" gutterBottom>
+          Web Developer
+        </Typography>
+        <Button variant="contained" color="secondary" disableElevation>
+          Check Out my Portfolio
+        </Button>
+        <Button variant="outlined" color="secondary">Check out my github</Button>
+
+
+        </Paper>
+
+
+        </div>
+
+                      <Image alt="a portrait of me" src={profile} />
+        <Image alt="a portrait of me" src={profile} />
+        <Image alt="a portrait of me" src={profile} />
+        <Image alt="a portrait of me" src={profile} />
       </main>
 
       <footer className={styles.footer}>
-                  <span className={styles.logo}>
-            <Image src="/logo1.svg" alt="Raffaele Cataldo Logo" width={140} height={32} />
-          </span>
+        <span className={styles.logo}>
+          <Image
+            src="/R.svg"
+            alt="Raffaele Cataldo Logo"
+            width={140}
+            height={32}
+          />
+        </span>
       </footer>
     </div>
-  )
+  );
 }
