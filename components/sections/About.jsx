3c9162e1a8ca1e05b2fe03457dda565fa8eb492f
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-02-24 14:43:57 +0100

    added portrait

--- components/sections/About.jsx
+++ components/sections/About.jsx
@@ -5,17 +5,11 @@ import portrait from "../../public/portrait1bw.JPG";
 const About = () => {
   return (
     <Box sx={{padding:8}} >
-      <Grid container spacing={8}>
+      <Grid container spacing={4}>
         <Grid item xs={12}>
           <Typography variant="h2">About me</Typography>
         </Grid>
-        {/* <Grid item xs={2}> */}
-        {/*         <Box > */}
-        {/*           <Image alt="a portrait of me" src={portrait} /> */}
-        {/*         </Box> */}
-        {/* </Grid> */}
-        <Grid item xs={1} />
-        <Grid item md={8} xs={10}>
+                 <Grid item md={8} xs={12}>
           <Typography variant="p">
             I've always enjoyed a challenge. As a professional musician, I
             played in a circus band and travelled from Istanbul to Singapore by
@@ -35,10 +29,14 @@ const About = () => {
             take on more responsibility and looking forward to becoming part of
             a cutting-edge team.
             <br />
-            In my spare time, I raise 2 children, bake bread, and still manage
-            to play music now and again. <br />
           </Typography>
         </Grid>
+<Grid item xs={8} md={4}> 
+                 <Box > 
+                   <Image alt="a portrait of me" src={portrait} />
+                 </Box> 
+         </Grid> 
+
       </Grid>
       <Box sx={{margin:4, textAlign:'center'}}> 
       <Button color="secondary" disableElevation variant="contained" sx={{ padding: 2 }}>Contact Me</Button>
