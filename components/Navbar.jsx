b5b38c812623cc50c218dd86e2f04f04b1216235
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-02-28 08:44:17 +0100

    git and linked links working

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,16 +11,18 @@ import Button from "@mui/material/Button";
 import MenuItem from "@mui/material/MenuItem";
 import Slide from "@mui/material/Slide";
 import useScrollTrigger from "@mui/material/useScrollTrigger";
-import Image from 'next/image';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import EmailIcon from '@mui/icons-material/Email';
+import Image from "next/image";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import EmailIcon from "@mui/icons-material/Email";
+import { Link as MuiLink } from '@mui/material';
 
-const xlinkedIn = <LinkedInIcon />;
-const xgitHub = <GitHubIcon/>;
+// Named x... because it seemed the menu was layed out alphabetically?
+const xlinkedIn = <a href="https://www.linkedin.com/in/raffaele-cataldo-dev/" rel="noreferrer" target="_blank" color="secondary"><LinkedInIcon /></a>;
+const xgitHub = <a href="https://github.com/farmeroy" rel="noreferrer" target="_blank" color="secondary"><GitHubIcon /></a>;
 const xcontact = <EmailIcon />;
 
-const pages = ["About", "Portfolio", "Resume", xgitHub, xlinkedIn , xcontact];
+const pages = ["About", "Portfolio", "Resume", xgitHub, xlinkedIn, xcontact];
 
 function HideOnScroll(props) {
   const { children, window } = props;
@@ -51,21 +53,24 @@ const Navbar = (props) => {
 
   return (
     <HideOnScroll {...props}>
-      <AppBar >
+      <AppBar>
         <Container maxWidth="xl">
           <Toolbar disableGutters>
-      
             <Typography
               variant="h6"
               color="secondary"
               noWrap
               component="div"
-              sx={{ mr: 2, display: { xs: "none", md: "flex" },  }}
+              sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
             >
               <Button color="secondary">
-
-              <Image src="/R.svg" alt="Raffaele Cataldo Logo" width={140} height={32} />
-              </Button>              
+                <Image
+                  src="/R.svg"
+                  alt="Raffaele Cataldo Logo"
+                  width={140}
+                  height={32}
+                />
+              </Button>
             </Typography>
 
             <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
@@ -98,8 +103,13 @@ const Navbar = (props) => {
                 }}
               >
                 {pages.map((page) => (
-                  <MenuItem key={pages.indexOf(page)} onClick={handleCloseNavMenu}>
-                    <Typography color="secondary" textAlign="center">{page}</Typography>
+                  <MenuItem
+                    key={pages.indexOf(page)}
+                    onClick={handleCloseNavMenu}
+                  >
+                    <Typography color="secondary" textAlign="center">
+                      {page}
+                    </Typography>
                   </MenuItem>
                 ))}
               </Menu>
@@ -111,9 +121,19 @@ const Navbar = (props) => {
               component="div"
               sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
             >
-<Image src="/R.svg" alt="Raffaele Cataldo Logo" width={140} height={32} />
+              <Image
+                src="/R.svg"
+                alt="Raffaele Cataldo Logo"
+                width={140}
+                height={32}
+              />
             </Typography>
-            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex", justifyContent:'flex-end'},}}>
+            <Box
+              sx={{
+                flexGrow: 1,
+                display: { xs: "none", md: "flex", justifyContent: "flex-end" },
+              }}
+            >
               {pages.map((page) => (
                 <Button
                   key={pages.indexOf(page)}
