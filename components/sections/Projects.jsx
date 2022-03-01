d3e9a974a17c2d752284ac7daaee73587e5032e8
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-03-01 16:36:32 +0100

    added link urls to projects

--- components/sections/Projects.jsx
+++ components/sections/Projects.jsx
@@ -7,35 +7,47 @@ const projectArr = [
     title: 'Meet App',
     img: '/meet-app.png',
     alt: 'An image of meet app',
-    description: 'A fullstack, progressive web app built with React and AWS Lambda. Users can access events via Google Calender API to search for web developer workshops across the globe, and also view data related to their search.Users sign in with GoogleAuth0'
+    description: 'A fullstack, progressive web app built with React and AWS Lambda. Users can access events via Google Calender API to search for web developer workshops across the globe, and also view data related to their search.Users sign in with GoogleAuth0',
+    gitUrl: 'https://github.com/farmeroy/meet-app',
+    liveUrl: 'https://farmeroy.github.io/meet-app'
   },
 {
     title: 'Pre-Code-Flix',
   img: '/movie-app.png',
     alt: 'A screenshot of the app',
-    description: "This is a 'IMDB style' movie app. .The front end is built with React, React-Router and React-Bootstrap.The back-end is built with Node, Passport, and MongoDB, Login with username: 'testUser', password: 'password' "  },{
+  description: "This is a 'IMDB style' movie app. .The front end is built with React, React-Router and React-Bootstrap.The back-end is built with Node, Passport, and MongoDB, Login with username: 'testUser', password: 'password' ",
+  gitUrl: 'https://github.com/farmeroy/Movie-App-Client',
+  liveUrl: "https://pre-code-flix.netlify.app"
+},{
     title: 'Mancala',
       img: '/mancala.png',
     alt: 'a screenshot of the app',
-    description: "This project is a two player strategy game made with React.  The game's logic takes advantage of the useContext hook. Aside from styling the board, writing the gameplay algorithm was the focus of this app, and an enjoyable challenge."
+  description: "This project is a two player strategy game made with React.  The game's logic takes advantage of the useContext hook. Aside from styling the board, writing the gameplay algorithm was the focus of this app, and an enjoyable challenge.",
+  gitUrl: 'https://github.com/farmeroy/mancala',
+  liveUrl: 'https://farmeroy.github.io/mancala/'
     },
   {
     title: 'Music Group Landing Page',
     img: '/goodnight.png',
     alt: 'a band playing music',
-    description: 'This is a landing page made for a Berlin based music group. It was developed with vanilla HTML/CSS and a bit of javascript to handle the scrolling and menu bar.'
+    description: 'This is a landing page made for a Berlin based music group. It was developed with vanilla HTML/CSS and a bit of javascript to handle the scrolling and menu bar.',
+    gitUrl: null,
+    liveUrl: "http://goodnightcircus.com"
   },
   {
     title: 'Chat-App',
     img: '/chat-app.png',
     alt: 'an image of an iphone screen using the app',
-    description: 'This is a mobile chat app. It was developed with React-Native, Expo, and Gifted Chat. The messages and images are stored in Firebase. Users can can send text messages, images, and their location to all other users in the room.'
+    description: 'This is a mobile chat app. It was developed with React-Native, Expo, and Gifted Chat. The messages and images are stored in Firebase. Users can can send text messages, images, and their location to all other users in the room.',
+    gitUrl: 'https://github.com/farmeroy/chat-app',
   },
   {
     title: 'Synthesizer Web App',
     img: '/noise-machine.png',
     alt: 'screen shot of the web app interface',
-    description: 'This project uses the JavaScript Web Audio API, vanilla JavaScript, and object oriented programming to construct a musical web app. You can play with large chords with multiple voicings.'
+    description: 'This project uses the JavaScript Web Audio API, vanilla JavaScript, and object oriented programming to construct a musical web app. You can play with large chords with multiple voicings.',
+    gitUrl: 'https://github.com/farmeroy/Noise-Machine',
+    liveUrl: 'https://farmeroy.github.io/Noise-Machine'
   }
 
 
