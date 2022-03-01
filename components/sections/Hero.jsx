8bb869c117cc9509eb049e6f344b84a3188df9db
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-03-01 15:22:30 +0100

    imported smoothScroll, added lnks

--- components/sections/Hero.jsx
+++ components/sections/Hero.jsx
@@ -1,8 +1,13 @@
 import * as React from 'react';
-import { Button, Typography, Box } from '@mui/material'
+import { Button, Typography, Box, Link } from '@mui/material'
 import styles from "../../styles/Home.module.css"
+import smoothScrollHandler from '../../utility/scroll-handler';
 
 const Hero = () => {
+
+const goToPortfolioHandler = () => {
+    smoothScrollHandler('Portfolio');
+  }
     return (
       <Box component="div" sx={{minHeight: "70vh"}}>
             <Typography
@@ -19,13 +24,14 @@ const Hero = () => {
             </Typography>
             <h2 className={styles.code}>I build elegant solutions for front end (and back end) technologies</h2>
             <div>
-            <Button variant="contained" color="secondary" disableElevation sx={{padding:2}}>
+              <Button onClick={()=> goToPortfolioHandler()} variant="contained" color="secondary" disableElevation sx={{padding:2}}>
               Check Out my Portfolio
             </Button>
             {'   '}
+            <Link underline='none' href='https://github.com/farmeroy' rel="noreferrer" target="_blank">
             <Button variant="outlined" color="secondary" sx={{padding:2}}>
               Check out my github
-            </Button>
+          </Button></Link>
             </div>
           </Box>
     )}
