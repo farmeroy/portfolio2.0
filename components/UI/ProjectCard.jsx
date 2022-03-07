0708bea64657c1705ab1c15701d9ad9b330b676f
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-03-07 21:01:37 +0100

    prettier

--- components/UI/ProjectCard.jsx
+++ components/UI/ProjectCard.jsx
@@ -1,40 +1,98 @@
-import { Card, Link, CardContent, CardMedia, CardActionArea, CardActions, Button, Typography } from '@mui/material'
-
+import {
+  Card,
+  Link,
+  CardContent,
+  CardMedia,
+  CardActionArea,
+  CardActions,
+  Button,
+  Typography,
+} from "@mui/material";
 
 const ProjectCard = (props) => {
- return (
-    <Card sx={{  height:"100%", display: "flex", flexDirection: "column", justifyContent: "space-between" }} variant="outlined" >
+  return (
+    <Card
+      sx={{
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+      }}
+      variant="outlined"
+    >
       <CardActionArea>
-        <Link underline='none' href={props.liveUrl ? props.liveUrl : props.gitUrl} target="_blank">
-      <CardMedia
-        component="img"
-        height="200"
-        image={props.img}
-        alt={props.alt}
-      />
-      <CardContent sx={{}}>
-        <Typography gutterBottom variant="h5" color="text.secondary" component="div" >
-          {props.title}
-        </Typography>
-        <Typography variant="body2" color="text.primary" >
-          {props.description}
-        </Typography>
-      </CardContent>
-      </Link>
-</CardActionArea>
-      <CardActions sx={{justifySelf: 'flex-end'}} >
-        {props.caseStudy &&
-        <Button size="small" color="secondary" variant="outlined"><Link underline='none' href={`/case-studies/${props.caseStudy}`} size="small" color="secondary" variant="outlined">Case Study</Link></Button> }
-        {props.liveUrl && 
-        <Button size="small" color="secondary" variant="outlined"><Link underline='none' href={props.liveUrl} target="_blank" size="small" color="secondary" variant="outlined">See Live</Link></Button> }
-      
-        {props.gitUrl &&
-        <Button size='small' color='secondary' variant='outlined'>
-        <Link underline='none' href={props.gitUrl} target="_blank" size="small" color="secondary" variant="outlined">See Code</Link>
-        </Button>}
+        <Link
+          underline="none"
+          href={props.liveUrl ? props.liveUrl : props.gitUrl}
+          target="_blank"
+        >
+          <CardMedia
+            component="img"
+            height="200"
+            image={props.img}
+            alt={props.alt}
+          />
+          <CardContent sx={{}}>
+            <Typography
+              gutterBottom
+              variant="h5"
+              color="text.secondary"
+              component="div"
+            >
+              {props.title}
+            </Typography>
+            <Typography variant="body2" color="text.primary">
+              {props.description}
+            </Typography>
+          </CardContent>
+        </Link>
+      </CardActionArea>
+      <CardActions sx={{ justifySelf: "flex-end" }}>
+        {props.caseStudy && (
+          <Button size="small" color="secondary" variant="outlined">
+            <Link
+              underline="none"
+              href={`/case-studies/${props.caseStudy}`}
+              size="small"
+              color="secondary"
+              variant="outlined"
+            >
+              Case Study
+            </Link>
+          </Button>
+        )}
+        {props.liveUrl && (
+          <Button size="small" color="secondary" variant="outlined">
+            <Link
+              underline="none"
+              href={props.liveUrl}
+              target="_blank"
+              size="small"
+              color="secondary"
+              variant="outlined"
+            >
+              See Live
+            </Link>
+          </Button>
+        )}
+
+        {props.gitUrl && (
+          <Button size="small" color="secondary" variant="outlined">
+            <Link
+              underline="none"
+              href={props.gitUrl}
+              target="_blank"
+              size="small"
+              color="secondary"
+              variant="outlined"
+            >
+              See Code
+            </Link>
+          </Button>
+        )}
       </CardActions>
     </Card>
   );
-}
+};
 
 export default ProjectCard;
