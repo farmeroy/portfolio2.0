ecc6924ef385395c9c450ce9b44bfd0db70fc249
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-03-07 15:47:10 +0100

    prettier

--- pages/index.js
+++ pages/index.js
@@ -6,42 +6,42 @@ import Hero from "../components/sections/Hero";
 import About from "../components/sections/About";
 import Projects from "../components/sections/Projects";
 import ContactForm from "../components/sections/Contact";
-import {Grid, Fab} from '@mui/material';
-import ScrollToTopBtn from '../components/UI/ScrollToTopBtn'
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-
-
+import { Grid, Fab } from "@mui/material";
+import ScrollToTopBtn from "../components/UI/ScrollToTopBtn";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
 export default function Home(props) {
   return (
     <div id="back-to-top-anchor">
       <Head>
         <title>Raffaele Cataldo - Developer</title>
-        <meta name="description" content="built by Raffaele Cataldo" />
+        <meta
+          name="description"
+          content="My awesome portfolio website built using NextJS and MUI"
+        />
         <link rel="icon" href="/R.svg" />
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <Navbar />
-      <Grid container >
-      <Grid item xs={1} xl={2} />
-      <Grid item xs={10} xl={8}>
-      <main className={styles.main}>
-        <div className={styles.hero}>
-          <Hero />
-        </div>
-        <About />
-        <Projects />
-        <ContactForm />
-        {"  "}
-      </main>
-      </Grid >
-      <Grid item xs={1} lg={2} />
-<ScrollToTopBtn {...props}>
-        <Fab color="secondary" size="small" aria-label="scroll back to top">
-          <KeyboardArrowUpIcon />
-        </Fab>
-      </ScrollToTopBtn>
-
+      <Grid container>
+        <Grid item xs={1} xl={2} />
+        <Grid item xs={10} xl={8}>
+          <main className={styles.main}>
+            <div className={styles.hero}>
+              <Hero />
+            </div>
+            <About />
+            <Projects />
+            <ContactForm />
+            {"  "}
+          </main>
+        </Grid>
+        <Grid item xs={1} lg={2} />
+        <ScrollToTopBtn {...props}>
+          <Fab color="secondary" size="small" aria-label="scroll back to top">
+            <KeyboardArrowUpIcon />
+          </Fab>
+        </ScrollToTopBtn>
       </Grid>
 
       <footer className={styles.footer}>
@@ -54,6 +54,6 @@ export default function Home(props) {
           />
         </span>
       </footer>
-          </div>
+    </div>
   );
 }
