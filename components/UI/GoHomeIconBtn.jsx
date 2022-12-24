5c09759fa1679da1467e72277299380cf661b3d0
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-12-23 23:44:36 -0700

    adjust size

--- components/UI/GoHomeIconBtn.jsx
+++ components/UI/GoHomeIconBtn.jsx
@@ -4,15 +4,9 @@ import { Button } from "@mui/material";
 
 const GoHomeIconBtn = () => {
   return (
-    <Button color="secondary" edge="start">
+    <Button color="secondary">
       <Link href="/" passHref>
-        <Image
-          color="primary"
-          src="/R.svg"
-          alt="Raffaele Cataldo Logo"
-          width={64}
-          height={64}
-        />
+        <Image height={48} width={48} src="/R.svg" alt="go home button" />
       </Link>
     </Button>
   );
