50637271df3f08a841ac95f0017e43aeba5fcc5d
Raffaele Cataldo <kemanicataldo@gmail.com>
2023-07-16 13:44:08 -0700

    work on copy

--- components/sections/About.jsx
+++ components/sections/About.jsx
@@ -10,32 +10,49 @@ const About = () => {
         </Grid>
         <Grid item md={8} xs={12}>
           <Typography variant="p">
-            I&apos;ve always enjoyed a challenge. As a professional musician, I
-            played in a circus band and travelled from Istanbul to Singapore by
-            bicycle. These days, my challenge is developing React apps using Vim
-            in my terminal!
+            I&apos;m a passionate web developer based in Northern California,
+            specializing in NextJS with TypeScript and experienced in working on
+            large-scale React codebases. My focus revolves around creating
+            intuitive UI components, tackling complex forms, and designing
+            robust database models and APIs to deliver exceptional user
+            experiences.
             <br />
             <br />
-            With years working in small groups and dedicating myself to
-            practice, I know how to work on a team and also how to maintain
-            discipline and focus in remote work. I&apos;ve built landing pages
-            for colleagues&apos; musical projects, built my own Rest APIs from
-            scratch, and developed single page applications with React. I&apos;m
-            currently developing UI for{" "}
-            <Link
-              href="https://www.withencore.com/"
-              target="_blank"
-              rel="noreferrer"
-              color="secondary"
-            >
-              Encore
-            </Link>
-            , a company helping creators manage their finances.
+            Previously a jazz violinist perpetually on tour, I came to web
+            development during the pandemic, when I found myself with all
+            together too much time on my hands. I discovered a creative and
+            intellectual outlet in programming, building websites for artists in
+            my community. Witnessing the impact of my work, I became deeply
+            committed to leveraging the power of the web to positively change
+            the world.
             <br />
-            <br />I love learning new things and having new experiences. If you
-            want to chat about your .vimrc, music theory, or anything related to
-            software and web development, send me a message or contact me on
-            LinkedIn.
+            <br />
+            <br />
+            <br />
+            I believe in the power of the web to change the world, and I&apos;m
+            excited to make an impact, and currently volunteer time to the
+            Radical Engineers project. Most recently working on a survey app for
+            workplace accessibility startup Sick in the City.
+            <br />
+            <br />
+            With years of collaborating in diverse ensembles and dedicating
+            myself to practice, I know how to work on a team and also how to
+            maintain discipline and focus in remote work.
+            <br />
+            <br />
+            I love learning new things and having new experiences. Currently,
+            that means digging into Rust, furthering my knowledge of databases,
+            and working towards a BSc of Computer Science through the Open
+            University.
+            <br />
+            <br />
+            If you&apos;re seeking a developer who brings creativity,
+            dedication, and a fresh perspective to your tech company&apos;s
+            mission, let&apos;s connect on LinkedIn. I&apos;m always open to
+            discussing .vimrc, music theory, or anything related to software and
+            web development. Let&apos;s make an impact together!
+            <br />
+            <br />
           </Typography>
         </Grid>
         <Grid item xs={6} md={4}>
