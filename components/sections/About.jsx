b2c3df3b8be9918547520b45e75b18c6a4bd27ad
Raffaele Cataldo <kemanicataldo@gmail.com>
2023-07-17 16:32:47 -0700

    update about

--- components/sections/About.jsx
+++ components/sections/About.jsx
@@ -10,29 +10,43 @@ const About = () => {
         </Grid>
         <Grid item md={8} xs={12}>
           <Typography variant="p">
-            I&apos;m a passionate web developer based in Northern California,
-            specializing in NextJS with TypeScript and experienced in working on
-            large-scale React codebases. My focus revolves around creating
-            intuitive UI components, tackling complex forms, and designing
-            robust database models and APIs to deliver exceptional user
-            experiences.
+            I&apos;m a web developer based in Northern California, specializing
+            in NextJS with TypeScript and experienced in working on large-scale
+            React codebases. My focus revolves around creating intuitive UI
+            components, tackling complex forms, and designing robust database
+            models and APIs to deliver exceptional user experiences.
             <br />
             <br />
             Previously a jazz violinist perpetually on tour, I came to web
-            development during the pandemic, when I found myself with all
-            together too much time on my hands. I discovered a creative and
-            intellectual outlet in programming, building websites for artists in
-            my community. Witnessing the impact of my work, I became deeply
-            committed to leveraging the power of the web to positively change
-            the world.
+            development during the pandemic, discovering a creative and
+            intellectual outlet in programming while building websites for
+            artists in my community. Witnessing the impact of that work, I
+            poured my heart into building and learning about web applications.
             <br />
-            <br />
-            <br />
-            <br />
-            I believe in the power of the web to change the world, and I&apos;m
-            excited to make an impact, and currently volunteer time to the
-            Radical Engineers project. Most recently working on a survey app for
-            workplace accessibility startup Sick in the City.
+            <br />I believe in the power of the web to change the world, and
+            I&apos;m excited to make an impact. My previous role was working at
+            an early stage startup (
+            <Link
+              href="https://www.withencore.com/"
+              target="_blank"
+              rel="noreferrer"
+              color="secondary"
+            >
+              WithEncore
+            </Link>
+            ), building financial tools for the entertainment industry to change
+            the way Hollywood does business. Not solely focused on finances, I
+            also volunteer time to{" "}
+            <Link
+              href="https://radicalengineers.com/"
+              target="_blank"
+              rel="noreferrer"
+              color="secondary"
+            >
+              Radical Engineers
+            </Link>
+            , most recently working on a survey app for workplace accessibility
+            startup Sick in the City.
             <br />
             <br />
             With years of collaborating in diverse ensembles and dedicating
@@ -48,9 +62,7 @@ const About = () => {
             <br />
             If you&apos;re seeking a developer who brings creativity,
             dedication, and a fresh perspective to your tech company&apos;s
-            mission, let&apos;s connect on LinkedIn. I&apos;m always open to
-            discussing .vimrc, music theory, or anything related to software and
-            web development. Let&apos;s make an impact together!
+            mission, let&apos; talk!
             <br />
             <br />
           </Typography>
